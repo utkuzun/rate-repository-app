533567404003d9d4545317cb33c7a72a7a3719a7
utkuzun <utkuzun50@gmail.com>
2022-09-30 16:46:06 +0300

    eslint adde

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style='ahmet'>
-        Open up App.js to start working on utku your app!
-      </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   )
